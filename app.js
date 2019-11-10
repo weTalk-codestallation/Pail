7fd3fde60f6c288e47101c7e39802e47d991a146
Louise Pei <luyaopei@brandeis.edu>
2019-11-09 21:34:34 -0500

    upload profile login stuff

---
+++ app.js
@@ -0,0 +1,199 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var apikey = require('./config/apikey');
+// AUTHENTICATION MODULES
+session = require("express-session"),
+bodyParser = require("body-parser"),
+User = require( './models/User' ),
+flash = require('connect-flash')
+// END OF AUTHENTICATION MODULES
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const mongoose = require( 'mongoose' );
+mongoose.connect( 'mongodb://localhost/test' , {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected!")
+});
+
+const profileController = require('./controllers/profileController')
+
+
+// Authentication
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// here we set up authentication with passport
+const passport = require('passport')
+const configPassport = require('./config/passport')
+configPassport(passport)
+
+
+
+var app = express();
+
+app.use(function(req,res,next){
+    console.log("about to make some routes")
+    next()
+});
+
+/*************************************************************************
+     HERE ARE THE AUTHENTICATION ROUTES
+**************************************************************************/
+
+app.use(session({ secret: 'zzbbyanana' }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+const approvedLogins = ["luyaopei@brandeis.edu"];
+
+// here is where we check on their logged in status
+app.use((req,res,next) => {
+  res.locals.title="Swatching"
+  res.locals.loggedIn = false
+  if (req.isAuthenticated()){
+    if (req.user.googleemail.endsWith("@brandeis.edu") ||
+          approvedLogins.includes(req.user.googleemail))
+          {
+            console.log("user has been Authenticated")
+            res.locals.user = req.user
+            res.locals.loggedIn = true
+          }
+    else {
+      res.locals.loggedIn = false
+    }
+    console.log('req.user = ')
+    console.dir(req.user)
+    // here is where we can handle whitelisted logins ...
+    if (req.user){
+      if (req.user.googleemail=='luyaopei@brandeis.edu'){
+        console.log("Owner has logged in")
+        res.locals.status = 'owner'
+      }else {
+        console.log('student has logged in')
+        res.locals.status = 'user'
+      }
+    }
+  }
+  next()
+})
+
+
+
+// here are the authentication routes
+
+app.get('/loginerror', function(req,res){
+  res.render('loginerror',{})
+})
+
+app.get('/login', function(req,res){
+  res.render('login',{})
+})
+
+
+
+// route for logging out
+app.get('/logout', function(req, res) {
+        req.session.destroy((error)=>{console.log("Error in destroying session: "+error)});
+        console.log("session has been destroyed")
+        req.logout();
+        res.redirect('/');
+    });
+
+
+// =====================================
+// GOOGLE ROUTES =======================
+// =====================================
+// send to google to do the authentication
+// profile gets us their basic information including their name
+// email gets their emails
+app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+
+app.get('/login/authorized',
+        passport.authenticate('google', {
+                successRedirect : '/',
+                failureRedirect : '/loginerror'
+        })
+      );
+
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    console.log("checking to see if they are authenticated!")
+    // if user is authenticated in the session, carry on
+    res.locals.loggedIn = false
+    if (req.isAuthenticated()){
+      console.log("user has been Authenticated")
+      res.locals.loggedIn = true
+      return next();
+    } else {
+      console.log("user has not been authenticated...")
+      res.redirect('/login');
+    }
+}
+
+// we require them to be logged in to see their profile
+app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile')/*, {
+            user : req.user // get the user out of session and pass to template
+        });*/
+    });
+
+app.get('/editProfile',isLoggedIn, (req,res)=>{
+    res.render('editProfile')
+})
+
+app.get('/profiles', isLoggedIn, profileController.getAllProfiles);
+app.get('/showProfile/:id', isLoggedIn, profileController.getOneProfile);
+
+
+
+app.post('/updateProfile',profileController.update)
+
+
+// END OF THE AUTHENTICATION ROUTES
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/', function(req, res, next) {
+  res.render('index',{title:"Swatching"});
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ config/apikey.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'apikey':{
+        zipcode: 'cUVbjjOln90RuYOF9rPQ99BjOQvSrMv6STV604FWVXHhlmxrYXZw3YOOIGs5Pznn'
+    }
+
+};
