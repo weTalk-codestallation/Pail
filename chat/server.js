13be1bb042d3be6825b4ab07bd44dc78102c4f19
Jane-An <jane.an.0426@gmail.com>
2019-11-10 03:27:53 -0500

    debug, the basics for chat feature is all set, waiting for polishing

--- chat/server.js
+++ chat/server.js
@@ -23,14 +23,15 @@ io.sockets.on('connection',function(socket){
 		users.splice(users.indexOf(socket.username),1);
 		io.sockets.emit('get users',users);
 
-		io.sockets.emit('new message',{msg:data,user:socket.username});
-		
+
+
 		connections.splice(connections.indexOf(socket),1);
 		console.log('Disonnected: %s sockets connected',connections.length);
 	});
 	
 //send message
 	socket.on('send message',function(data){
+		console.log(data);
 		io.sockets.emit('new message',{msg:data,user:socket.username});
 	});
 
