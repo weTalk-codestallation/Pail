8cc35bbc7e910a1e27c5d8786cf16e847505da70
Jane-An <jane.an.0426@gmail.com>
2019-11-10 03:57:07 -0500

    add readable comments to chat codes

--- chat/server.js
+++ chat/server.js
@@ -1,47 +1,43 @@
-var express = require('express');
+var express = require('express'); //framework
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 users = [];
-connections =[];
+changes =[];
 
+
+//In terminal, accounce that the pairpal chat feature is on 
 server.listen(process.env.PORT || 3000);
-console.log('PairPal running...');
+console.log('PairPal matching...');
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection',function(socket){
-	connections.push(socket);
-	console.log('Connected: %s sockets connected', connections.length);
+	changes.push(socket);
+	console.log('Processing: %s change', changes.length);//aim for debuging, show the number of commands
 
 
-//Disconnect
+//Usually, this part is not underused. It makes sure the login feature is disconnected so that the chat may not be interrupted
 	socket.on('disconnnect',function(data){
-
 		users.splice(users.indexOf(socket.username),1);
 		io.sockets.emit('get users',users);
-
-
-
-		connections.splice(connections.indexOf(socket),1);
-		console.log('Disonnected: %s sockets connected',connections.length);
+		changes.splice(changes.indexOf(socket),1);
 	});
 	
-//send message
+//Send message (add new message to messages' list)
 	socket.on('send message',function(data){
-		console.log(data);
-		io.sockets.emit('new message',{msg:data,user:socket.username});
+		console.log(data);//aim for debug 
+		io.sockets.emit('new message',{messageList:data,userList:socket.username});//messageList and userList are called in index.html
 	});
 
-//new user
+//Login (add new users to users' list)
 	socket.on('new user',function(data, callback){
 		callback(true);
 		socket.username = data;
-		console.log(socket.username);
+		console.log(socket.username);//check who have been added in terminal
 		users.push(socket.username);
 		io.sockets.emit('get users',users);
 	});
-
 });
